ce53fba14a2b22de3566ab42e7dac2fc128d51e5
kauangarciasasilva <kauangarciasasilva@gmail.com>
2023-06-26 19:04:55 -0300

    çlkjhg

---
+++ src/components/repositori-component.tsx
@@ -0,0 +1,12 @@
+import { View,Text } from "react-native";
+
+export default function RepositoriComponent(){
+  
+   
+        return (
+            <View>
+                <Text>jjjj</Text>
+            </View>
+        );
+    
+}
